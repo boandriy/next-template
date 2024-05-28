7065f637e47aeaa5372213210572cf3af0880e95
boandriy <borovetsandriy@gmail.com>
2024-05-28 11:16:42 +0300

    initialized hooks structure with basic hooks(screenWidth + clickOutside)

---
+++ src/hooks/useWindowSize.ts
@@ -0,0 +1,33 @@
+import { useEffect, useState } from 'react';
+
+interface Size {
+  width: number | undefined;
+  height: number | undefined;
+}
+function useWindowSize(): Size {
+  const isClient = typeof window === 'object';
+  const [windowSize, setWindowSize] = useState<Size>({
+    width: isClient ? window.innerWidth : undefined,
+    height: isClient ? window.innerHeight : undefined,
+  });
+
+  useEffect(() => {
+    if (!isClient) {
+      return;
+    }
+
+    function handleResize() {
+      setWindowSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    }
+
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  return windowSize;
+}
+
+export default useWindowSize;
